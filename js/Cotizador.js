dff805a672b3accd64baca5dce376d41e754fafb
danielorrante <orrante_daniel@hotmail.com>
2023-09-13 02:45:51 -0500

    Ya salió bien el cotizador dependiendo el tipo de destino es el precio por pasajero y se multiplica

--- js/Cotizador.js
+++ js/Cotizador.js
@@ -4,35 +4,63 @@
 let nombreCliente;
 let destino;
 let pax;
-let total = 0;
-
+let total;
+let ticket; 
 
 function motorFormulario() {
-    let destino = prompt('Ingrese su destino: Cancun, PDC o Tulum');
-    let pax = +prompt ('¿Cuántas personas nos acompañan? 1 a 10');
+    do {
+        destino = prompt('Ingrese su destino: Cancun, PDC o Tulum');
+        while ((destino != 'Cancun') && (destino != 'PDC') && (destino != 'Tulum')) {
+            destino = prompt('Ingrese BIEN su destino: Cancun, PDC o Tulum');
+        }
+        pax = +prompt ('¿Cuántas personas nos acompañan? 1 a 10');
+        while ((pax < 1) && (pax >= 10)) {
+        pax = +prompt('Ingrese Solamente de 1 a 10');
+        }
+    } while ((pax > 1) && (pax < 11));
 }
+
+function motorCotizador() {
+    if (destino == 'Cancun') {
+        return total = 15 * pax;
+    } else if (destino == 'PDC') {
+        return total = 25 * pax;
+    } else {
+        return total = 35 * pax;
+    }
+}
+
+
+nombreCliente = prompt ('Bienvenido al Cotizador de transporte \n Ingrese su nombre: ');
+
+motorFormulario();
+motorCotizador();
+
+alert ('Hola ' + nombreCliente + ', el precio por el transporte a ' + destino + ' para ' + pax + ' personas, es de $' + total + '\n Te pediría datos para confirmar pero estoy al límite :)');
+
+/*do {
+    destino = prompt('Ingrese su destino: Cancun, PDC o Tulum');
+    while ((destino !== 'Cancun') && (destino !== 'PDC') && (destino !== 'TULUM')) {
+        destino = prompt('Ingrese BIEN su destino: Cancun, PDC o Tulum');
+    }
+    pax = +prompt ('¿Cuántas personas nos acompañan? 1 a 10');
+    while ((pax < 1) && (pax >= 10)) {
+        pax = +prompt('Ingrese Solamente de 1 a 10');
+    }
+    nextStep = si
+} while (nextStep == SI)
+
+
 
 function motorCotizador (destino, pax) {
     if (destino == 'Cancun') {
-        let total = pax * $65;
+        let total = pax * 65;
         alert('Su transporte a Cancun tiene un precio de $' + total)
     } else if (destino == 'PDC'){
-        let total = pax * $99;
+        let total = pax * 99;
         alert('Su transporte a PDC tiene un precio de $' + total)
     } else if (destino == 'TTulum'){
-        let total = pax * $159;
+        let total = pax * 159;
         alert('Su transporte a Tulum tiene un precio de $' + total)
     } 
-}
-
-alert ("Bienvenido al Cotizador de transporte");
-
-motorFormulario ();
-
-do {
-    alert ("Ingrese un dato válido");
-    motorFormulario ();
-} while ( ((destino == 'Cancun') || (destino == 'PDC') || (destino == 'TULUM')) && ((pax > 0)|| (pax >= 10))) {
-    motorCotizador ();
-}
-
+} */
